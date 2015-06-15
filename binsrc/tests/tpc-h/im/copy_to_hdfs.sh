74f08c161476d130206741448ef1334753075b88
VOS Maintainer <vos.admin@openlinksw.com>
2015-06-15 03:53:16 -0400

    TPC-H Impala

--- binsrc/tests/tpc-h/im/copy_to_hdfs.sh
+++ binsrc/tests/tpc-h/im/copy_to_hdfs.sh
@@ -1,17 +1,17 @@
-hdfs dfs -mkdir /user/impala/tpch6data
-hdfs dfs -mkdir /user/impala/tpch6data/region
-hdfs dfs -put region.csv /user/impala/tpch6data/region
-hdfs dfs -mkdir /user/impala/tpch6data/supplier
-hdfs dfs -put supplier.csv /user/impala/tpch6data/supplier
-hdfs dfs -mkdir /user/impala/tpch6data/partsupp
-hdfs dfs -put partsupp.csv /user/impala/tpch6data/partsupp
-hdfs dfs -mkdir /user/impala/tpch6data/part
-hdfs dfs -put part.csv /user/impala/tpch6data/part
-hdfs dfs -mkdir /user/impala/tpch6data/orders
-hdfs dfs -put orders.csv /user/impala/tpch6data/orders
-hdfs dfs -mkdir /user/impala/tpch6data/nation
-hdfs dfs -put nation.csv /user/impala/tpch6data/nation
-hdfs dfs -mkdir /user/impala/tpch6data/customer
-hdfs dfs -put customer.csv /user/impala/tpch6data/customer
-hdfs dfs -mkdir /user/impala/tpch6data/lineitem
-hdfs dfs -put lineitem.csv /user/impala/tpch6data/lineitem
+hdfs dfs -mkdir /user/impala/tpch100data
+hdfs dfs -mkdir /user/impala/tpch100data/region
+hdfs dfs -put /data0/tpch100data/region.tbl /user/impala/tpch100data/region
+hdfs dfs -mkdir /user/impala/tpch100data/supplier
+hdfs dfs -put /data0/tpch100data/supplier.tbl.* /user/impala/tpch100data/supplier
+hdfs dfs -mkdir /user/impala/tpch100data/partsupp
+hdfs dfs -put /data0/tpch100data/partsupp.tbl.* /user/impala/tpch100data/partsupp
+hdfs dfs -mkdir /user/impala/tpch100data/part
+hdfs dfs -put /data0/tpch100data/part.tbl.* /user/impala/tpch100data/part
+hdfs dfs -mkdir /user/impala/tpch100data/orders
+hdfs dfs -put /data0/tpch100data/orders.tbl.* /user/impala/tpch100data/orders
+hdfs dfs -mkdir /user/impala/tpch100data/nation
+hdfs dfs -put /data0/tpch100data/nation.tbl /user/impala/tpch100data/nation
+hdfs dfs -mkdir /user/impala/tpch100data/customer
+hdfs dfs -put /data0/tpch100data/customer.tbl.* /user/impala/tpch100data/customer
+hdfs dfs -mkdir /user/impala/tpch100data/lineitem
+hdfs dfs -put /data0/tpch100data/lineitem.tbl.* /user/impala/tpch100data/lineitem
--- binsrc/tests/tpc-h/im/dbgen.sh
+++ binsrc/tests/tpc-h/im/dbgen.sh
@@ -1,109 +1,113 @@
-ssh -i mspasic.pem a1 mkdir /data0/tpch100data
-scp -i mspasic.pem dbgen a1:/data0/tpch100data
-scp -i mspasic.pem dists.dss a1:/data0/tpch100data
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -U 18 -i 4 -d 4)" &
+ssh -t -i mspasic.pem 10.0.1.251 sudo chmod 777 /data?
+ssh -i mspasic.pem 10.0.1.251 mkdir /data0/tpch100data
+scp -i mspasic.pem dbgen 10.0.1.251:/data0/tpch100data
+scp -i mspasic.pem dists.dss 10.0.1.251:/data0/tpch100data
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -U 18 -i 4 -d 4)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 1)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 1)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 2)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 2)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 3)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 3)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 4)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 4)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 5)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 5)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 6)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 6)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 7)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 7)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 8)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 8)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 9)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 9)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 10)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 10)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 11)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 11)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 12)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 12)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 13)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 13)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 14)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 14)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 15)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 15)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 16)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 16)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 17)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 17)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 18)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 18)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 19)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 19)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 20)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 20)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 21)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 21)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 22)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 22)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 23)" &
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 23)" &
 sleep 1
-ssh -i mspasic.pem a1 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 24)" &
-ssh -i mspasic.pem a2 mkdir /data0/tpch100data
-scp -i mspasic.pem dbgen a2:/data0/tpch100data
-scp -i mspasic.pem dists.dss a2:/data0/tpch100data
+ssh -i mspasic.pem 10.0.1.251 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 24)" &
+ssh -t -i mspasic.pem 10.0.1.250 sudo chmod 777 /data?
+ssh -i mspasic.pem 10.0.1.250 mkdir /data0/tpch100data
+scp -i mspasic.pem dbgen 10.0.1.250:/data0/tpch100data
+scp -i mspasic.pem dists.dss 10.0.1.250:/data0/tpch100data
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 25)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 25)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 26)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 26)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 27)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 27)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 28)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 28)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 29)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 29)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 30)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 30)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 31)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 31)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 32)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 32)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 33)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 33)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 34)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 34)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 35)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 35)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 36)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 36)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 37)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 37)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 38)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 38)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 39)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 39)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 40)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 40)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 41)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 41)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 42)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 42)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 43)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 43)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 44)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 44)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 45)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 45)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 46)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 46)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 47)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 47)" &
 sleep 1
-ssh -i mspasic.pem a2 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 48)" &
+ssh -i mspasic.pem 10.0.1.250 "(cd /data0/tpch100data; ./dbgen -q -f -s 100 -C 48 -S 48)" &
 wait
-ssh -i mspasic.pem a1 chmod -R 777  /data0/tpch100data
-ssh -i mspasic.pem a2 chmod -R 777  /data0/tpch100data
-ssh -i mspasic.pem a2 rm  /data0/tpch100data/nation.tbl /data0/tpch100data/region.tbl
-ssh -i mspasic.pem a1 "scp -i mspasic.pem /data0/tpch100data/*.u*.3 a2:/data0/tpch100data "
-ssh -i mspasic.pem a1 "scp -i mspasic.pem /data0/tpch100data/*.u*.4 a2:/data0/tpch100data "
+ssh -i mspasic.pem 10.0.1.251 chmod -R 777  /data0/tpch100data
+ssh -i mspasic.pem 10.0.1.250 chmod -R 777  /data0/tpch100data
+ssh -i mspasic.pem 10.0.1.250 rm  /data0/tpch100data/nation.tbl /data0/tpch100data/region.tbl
+ssh -i mspasic.pem 10.0.1.251 "scp -i mspasic.pem /data0/tpch100data/*.u*.3 10.0.1.250:/data0/tpch100data "
+ssh -i mspasic.pem 10.0.1.251 "scp -i mspasic.pem /data0/tpch100data/*.u*.4 10.0.1.250:/data0/tpch100data "
+ssh -i mspasic.pem 10.0.1.251 rm /data0/tpch100data/*.u*
+ssh -i mspasic.pem 10.0.1.250 rm /data0/tpch100data/*.u*
